9dbe3ab3c18d8cbb58ea0adffc0bbd53b954430a
Joep Kockelkorn <joepkockelkorn@gmail.com>
2022-06-02 20:37:41 +0200

    implement button

--- libs/shared/ui/button/src/lib/fancy-button/fancy-button.component.ts
+++ libs/shared/ui/button/src/lib/fancy-button/fancy-button.component.ts
@@ -2,8 +2,32 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'storybook-ui-testing-fancy-button',
-  template: ` <p>fancy-button works!</p> `,
-  styles: [],
+  template: `<button
+    [style.background]="colors[colorIndex]"
+    (click)="onClick()"
+  >
+    My fancy button 🦄
+  </button>`,
+  styles: [
+    `
+      button {
+        padding: 0.5em 1em;
+        color: white;
+        font-size: 16px;
+        border: 1px solid;
+        cursor: pointer;
+      }
+    `,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FancyButtonComponent {}
+export class FancyButtonComponent {
+  readonly colors = ['#1e7145', '#ee1111', '#2d89ef'];
+  colorIndex = 0;
+
+  onClick(): void {
+    const nextIndex = this.colorIndex + 1;
+    const maxIndex = this.colors.length - 1;
+    this.colorIndex = nextIndex > maxIndex ? 0 : nextIndex;
+  }
+}
