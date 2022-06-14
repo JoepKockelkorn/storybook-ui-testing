fd12e58856f77b6b98c214af15e7c68a05054cdb
Joep Kockelkorn <joepkockelkorn@gmail.com>
2022-06-14 10:39:39 +0200

    feat: add e2e test for fancy-button in app

--- apps/app-e2e/src/integration/app.spec.ts
+++ apps/app-e2e/src/integration/app.spec.ts
@@ -1,13 +1,16 @@
-import { getGreeting } from '../support/app.po';
+import { getFancyButton, getGreeting } from '../support/app.po';
 
 describe('app', () => {
-  beforeEach(() => cy.visit('/'));
+  beforeEach(() => {
+    cy.visit('/');
+    cy.login('my-email@something.com', 'myPassword');
+  });
 
   it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
     getGreeting().contains('Welcome app');
+  });
+
+  it('should display fancy button', () => {
+    getFancyButton().should('be.visible');
   });
 });
