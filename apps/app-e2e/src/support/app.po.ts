e21a1ec04dc0191b90c3752d19cb30ada1e3b86e
Joep Kockelkorn <joepkockelkorn@gmail.com>
2022-06-14 10:50:09 +0200

    ci: add workflow (#1)

--- apps/app-e2e/src/support/app.po.ts
+++ apps/app-e2e/src/support/app.po.ts
@@ -1,1 +1,2 @@
 export const getGreeting = () => cy.get('h1');
+export const getFancyButton = () => cy.get('storybook-ui-testing-fancy-button');
--- apps/app/src/app/app.module.ts
+++ apps/app/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SharedUiButtonModule } from '@storybook-ui-testing/shared/ui/button';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, SharedUiButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
